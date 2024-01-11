54607e21d9f1643971eb48036c62055da75a7f21
Tim Silva <tim17silva@gmail.com>
2024-01-11 12:22:05 -0800

    Beginning form

--- client/src/pages/Order.tsx
+++ client/src/pages/Order.tsx
@@ -2,6 +2,15 @@ export default function Order() {
   return (
     <div className="w-8/12 m-auto">
       <span className="text-2xl font-playfair">Order</span>
+      <form action="">
+        <input
+          type="text"
+          name=""
+          id=""
+          className=" bg-slate-300"
+          placeholder="First Name"
+        />
+      </form>
     </div>
   );
 }
