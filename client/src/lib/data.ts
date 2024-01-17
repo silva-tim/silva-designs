0093a94cd4a768cd25cae9eaeda80d6e1096be26
Tim Silva <tim17silva@gmail.com>
2024-01-17 15:31:18 -0800

    Adding fake data

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -2,12 +2,30 @@ import { Candle } from "../types/Candle";
 
 export const candles: Candle[] = [
   {
-    name: 'Herbal Serenity',
+    name: "Blooming Bliss",
     id: 1,
-    price: ,
-    image: ,
-    alt: ,
-    desc: ,
-    favorite: 
-  }
-]
+    price: 15,
+    image: "",
+    alt: "Blooming Bliss Candle",
+    desc: "A strong floral fragrance that effortlessly captures the essence of a gardenia bloom. This scent is a sensory journey where each whiff unveils a pure and enchanting aroma with hints of jasmine, rose, and lily.",
+    favorite: true,
+  },
+  {
+    name: "Festive Melody",
+    id: 2,
+    price: 15,
+    image: "",
+    alt: "Festive Melody Candle",
+    desc: "A delightful tapestry of festive aromas, weaving together the bold warmth of red cinnamon with the inviting embrace of spiced apple and citrusy orange notes capturing the essence of holiday cheer.",
+    favorite: true,
+  },
+  {
+    name: "Herbal Serenity",
+    id: 3,
+    price: 15,
+    image: "/images/products/herbal_serenity.jpg",
+    alt: "Herbal Serenity Candle",
+    desc: "The very essence of a lavender field in full bloom has been captured for your enjoyment. As the fragrance unfolds, the herbal symphony of sage introduces a touch of earthiness, grounding the lavender's ethereal quality.",
+    favorite: true,
+  },
+];
