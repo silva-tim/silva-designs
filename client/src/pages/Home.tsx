0f5415dea1a82d37acf69bafa727de287ef9bf97
Tim Silva <tim17silva@gmail.com>
2024-01-17 18:37:09 -0800

    Testing hero image changes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="w-10/12 m-auto">
       <div>
-        <img src="/images/backgrounds/hero.jpg" alt="" />
+        <img src="/images/backgrounds/hero2.jpeg" alt="" />
       </div>
       <Favorites />
     </div>
