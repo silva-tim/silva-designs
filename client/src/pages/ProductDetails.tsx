be8047f11ff7153550ac927195801e024785f28d
Tim Silva <tim17silva@gmail.com>
2024-02-08 14:26:20 -0800

    Working on details page

--- client/src/pages/ProductDetails.tsx
+++ client/src/pages/ProductDetails.tsx
@@ -18,11 +18,11 @@ export default function ProductDetails() {
       <div className="basis-2/5">
         <img src={candle?.image} alt={candle?.alt} />
       </div>
-      <div className="flex flex-wrap basis-3/5">
-        <div className="flex items-center justify-center border-b border-black h-36 basis-full">
+      <div className="flex flex-col basis-3/5">
+        <div className="flex items-center justify-center h-24 border-b border-black">
           <span className="text-2xl">{candle?.name}</span>
         </div>
-        <div>
+        <div className="flex">
           <p>{candle?.desc}</p>
         </div>
       </div>
