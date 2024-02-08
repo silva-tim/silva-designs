ac9dc2a34706754e8c82452c9ae2c8621b7e105b
Tim Silva <tim17silva@gmail.com>
2024-02-08 15:33:19 -0800

    Working on order form, deciding on state names for values

--- client/src/pages/Order.tsx
+++ client/src/pages/Order.tsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
+
 export default function Order() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [prefEmail, setPrefEmail] = useState();
+  const [prefText, prefText] = useState();
+
   return (
-    <div className="w-8/12 m-auto">
+    <section className="w-8/12 m-auto">
       <span className="text-2xl font-playfair">Order</span>
-      <form action="">
+      <form>
         <div className="flex flex-wrap">
           <span className="basis-full">Name</span>
           <div className="flex gap-5 basis-full">
             <input
               type="text"
-              name=""
-              id=""
+              name="firstName"
               className=" bg-slate-300 basis-1/2"
-              placeholder="First Name"
+              placeholder="First"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
             <input
               type="text"
-              name=""
-              id=""
+              name="lastName"
               className=" bg-slate-300 basis-1/2"
-              placeholder="Last Name"
+              placeholder="Last"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
             />
           </div>
         </div>
@@ -29,21 +40,25 @@ export default function Order() {
               type="email"
               className="bg-slate-300 basis-1/2"
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="tel"
               className="bg-slate-300 basis-1/2"
               placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
             />
           </div>
           <span>Preferred method of communication:</span>
-          <input type="radio" id="email" name="contact" />
+          <input type="radio" id="email" name="contact" checked={} />
           <label htmlFor="email">Email</label>
-          <input type="radio" id="text" name="contact" />
-          <label htmlFor="email">Text</label>
+          <input type="radio" id="phone" name="contact" />
+          <label htmlFor="phone">Text</label>
         </div>
         <button className="w-full bg-red-300">Add Product</button>
       </form>
-    </div>
+    </section>
   );
 }
