413619907b3ed5b5f8677181257146847121f0bf
Tim Silva <tim17silva@gmail.com>
2024-02-10 15:39:13 -0800

    Working on order form

--- client/src/components/ProductLine.tsx
+++ client/src/components/ProductLine.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { getCandles } from "../lib/Api";
+import { Candle } from "../types/Candle";
+
 export default function ProductLine() {
+  const [candles, setCandles] = useState<Candle[]>();
+  const [product, setProduct] = useState<string>();
+
+  useEffect(() => {
+    setCandles(getCandles());
+  }, []);
+
+  console.log(product);
   return (
-    <div className="h-20 border border-black bg-slate-400">dwinandwnai</div>
+    <div className="h-20 border border-black bg-slate-400">
+      <select name="" id="" onChange={(e) => setProduct(e.target.value)}>
+        {candles?.map((candle) => (
+          <option value={candle.name} key={candle.id}>
+            {candle.name}
+          </option>
+        ))}
+      </select>
+      <input type="checkbox" name="" id="" />
+    </div>
   );
 }
