39cd4ba0b9b535608942bfd252a3307c70b4364c
Tim Silva <tim17silva@gmail.com>
2024-02-14 14:38:46 -0800

    Trying new order form

--- src/lib/OrderContext.tsx
+++ src/lib/OrderContext.tsx
@@ -1,13 +1,87 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
-export const OrderContext = createContext(null);
+type OrderContextValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  prefContact: string;
+  onSetFirst: (first: string) => void;
+  onSetLast: (last: string) => void;
+  onSetEmail: (email: string) => void;
+  onSetPhone: (phone: string) => void;
+  onSetPref: (pref: string) => void;
+};
 
-export default function OrderContext({ children }) {
+const OrderContext = createContext<OrderContextValues>({
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  prefContact: "",
+  onSetFirst: () => undefined,
+  onSetLast: () => undefined,
+  onSetEmail: () => undefined,
+  onSetPhone: () => undefined,
+  onSetPref: () => undefined,
+});
+
+type props = {
+  children: ReactNode;
+};
+
+export default function OrderContextProvider({ children }: props) {
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [prefContact, setPrefContact] = useState<string>("Email");
 
-  return <OrderContext.Provider>{children}</OrderContext.Provider>;
+  function onSetFirst(first: string) {
+    setFirstName(first);
+  }
+
+  function onSetLast(last: string) {
+    setLastName(last);
+  }
+
+  function onSetEmail(email: string) {
+    setEmail(email);
+  }
+
+  function onSetPhone(phone: string) {
+    setPhoneNumber(phone);
+  }
+
+  function onSetPref(pref: string) {
+    setPrefContact(pref);
+  }
+
+  return (
+    <OrderContext.Provider
+      value={{
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        prefContact,
+        onSetFirst,
+        onSetLast,
+        onSetEmail,
+        onSetPhone,
+        onSetPref,
+      }}
+    >
+      {children}
+    </OrderContext.Provider>
+  );
+}
+
+export function useOrder() {
+  const context = useContext(OrderContext);
+  if (!context) {
+    throw new Error("useOrder can only be used inside OrderContextProvider");
+  }
+
+  return context;
 }
