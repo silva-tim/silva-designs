a9b3a7cecdb260b1f06aed9ae158d3e3891be8db
Tim Silva <tim17silva@gmail.com>
2024-02-22 12:57:44 -0800

    Adding info to pricing page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -5,16 +5,27 @@ export default function Pricing() {
         <span className="font-playfair text-2xl">Pricing & Info</span>
       </div>
       <div className="flex flex-wrap justify-center text-lg">
-        <p className="basis-2/3 py-5 text-center">
+        <span className="basis-full pt-5 text-center text-lg underline">
+          Information & Ingredients
+        </span>
+        <p className="basis-2/3 py-2 text-center">
           Each candle is handmade to order and contains only U.S. made
           ingredients including 100% phthalate free fragrances and an
           all-natural coconut wax that emits no carcinogens.
         </p>
-        <p className="basis-2/3 py-5 text-center">
+        <span className="basis-full pt-5 text-center text-lg underline">
+          Pricing
+        </span>
+        <p className="basis-2/3 py-2 text-center">
           All fragrances start at $25 for a 16 oz. matte black candle jar &
-          bamboo lid. 16 hour burn time with proper use. $30 Candle jar is
-          presented in a gift ready black box with white bow. Includes
-          complimentary bottle of matchsticks.
+          bamboo lid. Candles have an approximate 16 hour burn time with proper
+          use.
+        </p>
+        <p className="basis-2/3 py-2 text-center">
+          Every fragance has the option of being upgraded to a gift for $5 more.
+          Gifts will come with the candle jar presented in a gift ready black
+          box with a white bow. It also includes a complimentary bottle of
+          matchsticks.
         </p>
       </div>
     </section>
