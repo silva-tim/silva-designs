0a8902c11e7f06f4a17fef66dc25cd26bc1476d1
Tim Silva <tim17silva@gmail.com>
2024-02-29 12:23:28 -0800

    Working on medium sizing the footer. Adding extra links that will need to be added to mobile/small view

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -16,7 +16,7 @@ export default function ProductCard({
   return (
     <Link
       to={`/fragrances/${prodId}`}
-      className="group flex basis-1/2 flex-wrap justify-center pb-2 sm:basis-1/3"
+      className="group flex basis-1/2 flex-wrap justify-center pb-2 sm:basis-1/3 md:basis-1/5"
     >
       <div className="h-5/6 basis-full">
         <img
